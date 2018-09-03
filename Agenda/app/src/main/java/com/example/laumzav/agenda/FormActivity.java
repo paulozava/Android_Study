e623c09ed19cc6b6cc80c12d1e9f9ee2629cc304
paulozava <camposzava@gmail.com>
2018-09-03 19:14:02 +0200

    add toast to activity form

--- Agenda/app/src/main/java/com/example/laumzav/agenda/FormActivity.java
+++ Agenda/app/src/main/java/com/example/laumzav/agenda/FormActivity.java
@@ -1,7 +1,13 @@
 package com.example.laumzav.agenda;
 
+import android.support.v4.widget.PopupMenuCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        Button saveButton = (Button) findViewById(R.id.form_saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
