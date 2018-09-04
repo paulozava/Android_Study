47db66349e54bc2b782dca6f173144e2f6a640a8
paulozava <camposzava@gmail.com>
2018-09-04 16:14:59 +0200

    modeling DataBase for Jobs

--- Agenda/app/src/main/java/com/example/laumzav/agenda/dao/jobsDAO.java
+++ Agenda/app/src/main/java/com/example/laumzav/agenda/dao/jobsDAO.java
@@ -1,6 +1,23 @@
 package com.example.laumzav.agenda.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class jobsDAO extends SQLiteOpenHelper {
+
+    public jobsDAO(Context context) {
+        super(context, "Jobs", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Jobs (id INTEGER PRIMARY KEY, jobName TEXT NOT NULL, jobDesc TEXT, priority REAL, isDeletable TEXT);";
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
