3cdd2a9065a07d55e8931f82a70b14961c346f26
paulozava <camposzava@gmail.com>
2018-09-05 15:30:10 +0200

    seriazible Job Class

--- Agenda/app/src/main/java/com/example/laumzav/agenda/models/Jobs.java
+++ Agenda/app/src/main/java/com/example/laumzav/agenda/models/Jobs.java
@@ -1,6 +1,8 @@
 package com.example.laumzav.agenda.models;
 
-public class Jobs {
+import java.io.Serializable;
+
+public class Jobs implements Serializable {
     private int id;
     private String name;
     private String desc;
